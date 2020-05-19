0e7b84350966f6277d33b12b48691cb32ecbe5da
Basma Alafifi <alafifi.basma7@gmail.com>
2020-05-19 17:31:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+        function displayRadioValue() { 
+            var radio = document.getElementsByName('type'); 
+              
+            for(i = 0; i < radio.length; i++) { 
+                if(radio[i].checked) 
+                document.getElementById("result").innerHTML
+                        = "you are "+radio[i].value; 
+            } 
+    if (radio[i].val() ===document.getElementById('#witch') );
+    	{ function show ()
+{
+   var img=$('<img>');
+ $('#body').append(img);
+img.attr('src','http:huben.us/mediawiki/images/2/23/Welcome_To_Hogwarts.jpg')
+ ;
+        	}
+        
+}
+
+}
+
+
